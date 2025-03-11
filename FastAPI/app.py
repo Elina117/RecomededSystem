a97d8df2f0871df4cdad4bf6d4fc8c0466f2c7e3
Elina117 <106472257+Elina117@users.noreply.github.com>
2025-03-12 00:38:50 +0300

    change app for AB test with two my models to compare

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -1,13 +1,14 @@
 import os
 import pickle
+import hashlib
 from typing import List
 from datetime import datetime
 
 import pandas as pd
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from schema import PostGet
+from pydantic import BaseModel
 
 # Установка соединения с базой данных
 DATABASE_URL = (
@@ -16,17 +17,39 @@ DATABASE_URL = (
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+salt = 'my_salt'
+threshold = 50
 
+#Функция для определения групп для A/B тестирования
+def get_exp_group(user_id: int) -> str:
+    hash_value = int(hashlib.md5((str(user_id) + salt).encode()).hexdigest(), 16)
+    return 'control' if (hash_value % 100) < threshold else 'test'
+
+class PostGet(BaseModel):
+    id: int
+    text: str
+    topic: str
+
+    class Config:
+        orm_model = True
+
+class Response(BaseModel):
+    exp_group: str
+    recommendations: List[PostGet]
 
 # Функция для получения пути к модели
-def get_model_path(path: str) -> str:
-    if os.environ.get("IS_LMS") == "1":
-        return "/workdir/user_input/model"
-    return path
+def get_model_path(model_version: str) -> str:
+    if (
+        os.environ.get("IS_LMS") == "1"
+    ):  # проверяем где выполняется код в лмс, или локально. Немного магии
+        model_path = f"/workdir/user_input/model_{model_version}"
+    else:
+        model_path = "your/path/to/model"
+    return model_path
 
 # Загрузка модели
-def load_models():
-    model_path = get_model_path("/my/super/path")
+def load_models(model_name: str):
+    model_path = get_model_path(model_name)
     with open(model_path, "rb") as model_file:
         model = pickle.load(model_file)
 
@@ -54,7 +77,9 @@ def load_features() -> pd.DataFrame:
 app = FastAPI()
 
 # Загрузка модели и данных
-model = load_models()
+model_test = load_models("test")
+model_control = load_models("control")
+
 user = load_features()
 posts = pd.read_sql(
     """
@@ -68,32 +93,61 @@ def get_db():
     with SessionLocal() as db:
         yield db
 
-@app.get("/post/recommendations/", response_model=List[PostGet])
-def recommended_posts(id: int, time: datetime, limit: int = 5) -> List[PostGet]:
+
+@app.get("/post/recommendations/", response_model=Response)
+def recommended_posts(id: int, time: datetime, limit: int = 5) -> Response:
     # Загружаем данные о пользователе
+    exp_group = get_exp_group(id)
 
     user_data = user[user['user_id'] == id]
 
+    if user_data.empty:
+        raise ValueError("Пользователь не найден")
+
     # Формируем данные для предсказания
-    data = posts.join(user_data, how="cross")[
+    data_test = posts.join(user_data, how="cross")[
         ["city", "country", "text_y", "topic_y", "most_frequent_word_y",
          "gender","age", "exp_group", 'emb_0', 'emb_1', 'emb_2', 'emb_3', 'emb_4',
          "length_text", "max_tfidf",
          "has_numbers", "text_length_more1500", "cnt_actions", "os_iOS", "os_organic"]
     ]
 
-    # Предсказания вероятностей
-    data["pred"] = model.predict_proba(data)[:, 1]
+    data_control = posts.join(user_data, how="cross")[
+        ["city", "country", "text_y", "topic_y", "most_frequent_word_y",
+         "gender","age", "exp_group", "length_text", "max_tfidf",
+         "has_numbers", "text_length_more1500", "cnt_actions", "os_iOS", "os_organic"]
+    ]
 
-    # Сортируем по вероятностям и выбираем топ-N
-    data["post_id"] = posts["post_id"]
-    top_posts = data.sort_values(by="pred", ascending=False).head(limit)
-    indexes = list(top_posts["post_id"])
+    if exp_group == 'control':
+        # Предсказания вероятностей
+        data_control["pred"] = model_control.predict_proba(data_control)[:, 1]
 
-    # Получаем посты из pandas DataFrame по отобранным индексам
-    query = posts[posts['post_id'].isin(indexes)]
+        # Сортируем по вероятностям и выбираем топ-N
+        data_control["post_id"] = posts["post_id"]
+        top_posts = data_control.sort_values(by="pred", ascending=False).head(limit)
+        indexes = list(top_posts["post_id"])
 
-    # Выбираем только нужные столбцы (id, text, topic)
-    result = query[['post_id', 'text', 'topic']].rename(columns={'post_id': 'id'}).to_dict(orient='records')
+        # Получаем посты из pandas DataFrame по отобранным индексам
+        query = posts[posts['post_id'].isin(indexes)]
 
-    return result
+        # Выбираем только нужные столбцы (id, text, topic)
+        recommendations = query[['post_id', 'text', 'topic']].rename(columns={'post_id': 'id'}).to_dict(orient='records')
+
+    elif exp_group == 'test':
+        # Предсказания вероятностей
+        data_test["pred"] = model_test.predict_proba(data_test)[:, 1]
+
+        # Сортируем по вероятностям и выбираем топ-N
+        data_test["post_id"] = posts["post_id"]
+        top_posts = data_test.sort_values(by="pred", ascending=False).head(limit)
+        indexes = list(top_posts["post_id"])
+
+        # Получаем посты из pandas DataFrame по отобранным индексам
+        query = posts[posts['post_id'].isin(indexes)]
+
+        # Выбираем только нужные столбцы (id, text, topic)
+        recommendations = query[['post_id', 'text', 'topic']].rename(columns={'post_id': 'id'}).to_dict(orient='records')
+    else:
+        raise ValueError('Неизвестная группа эксперимента')
+
+    return Response(exp_group=exp_group, recommendations=recommendations)
